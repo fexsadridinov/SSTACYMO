b9c63632359c56afbe2bd319f9b65bdf0d7071ba
Fakhridin Sadridinov <fexssd@Fakhridins-MacBook-Pro.local>
2025-03-09 21:15:47 -0400

    Update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,31 +1,37 @@
 const Footer = () => {
     return (
-        <section className="c-space pt-7 pb-3 border-t
-        border-black-300 flex justify-between items-center
-         flex-wrap gap-5">
-            <div className="text-amber-700 flex gap-2">
-                <p>Terms & Conditions</p>
-                <p>|</p>
-                <p>Privacy Policy</p>
-            </div>
+        <section className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
             <div className="flex gap-3">
-                <div className="social-icon">
+                <a
+                    href="https://www.instagram.com/sstacymo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-icon"
+                >
                     <img
-                    src="/assets/instagram.svg"
-                    alt="instagram"
-                    className="w-1/2 h-1/2" />
-                </div>
-                <div className="social-icon">
+                        src="/assets/instagram.svg"
+                        alt="Instagram"
+                        className="w-1/2 h-1/2"
+                    />
+                </a>
+                <a
+                    href="https://www.threads.net/@sstacymo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-icon"
+                >
                     <img
                         src="/assets/thread.svg"
-                        alt="threads"
-                        className="w-1/2 h-1/2" />
-                </div>
+                        alt="Threads"
+                        className="w-1/2 h-1/2"
+                    />
+                </a>
             </div>
             <p className="text-amber-700">
                 &copy; 2023 SSTACYMO. All rights reserved.
             </p>
         </section>
-    )
-}
-export default Footer
+    );
+};
+
+export default Footer;
